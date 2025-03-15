54f4bba9aef44cc3e348019f91418a0765939575
taramunde1974 <120246309+taramunde1974@users.noreply.github.com>
2025-03-15 18:10:08 +0100

    Create javaredessociales.js

---
+++ javaredessociales.js
@@ -0,0 +1,19 @@
+
+  const urlToShare = window.location.href;
+        const textToShare = "Mira este contenido interesante";
+
+        document.getElementById('share-facebook').onclick = function() {
+            window.open(`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(urlToShare)}`, '_blank');
+        }
+
+        document.getElementById('share-twitter').onclick = function() {
+            window.open(`https://twitter.com/intent/tweet?text=${encodeURIComponent(textToShare)}&url=${encodeURIComponent(urlToShare)}`, '_blank');
+        }
+
+        document.getElementById('share-whatsapp').onclick = function() {
+            window.open(`https://api.whatsapp.com/send?text=${encodeURIComponent(textToShare)}%20${encodeURIComponent(urlToShare)}`, '_blank');
+        }
+
+        document.getElementById('share-telegram').onclick = function() {
+            window.open(`https://t.me/share/url?url=${encodeURIComponent(urlToShare)}&text=${encodeURIComponent(textToShare)}`, '_blank');
+        }
