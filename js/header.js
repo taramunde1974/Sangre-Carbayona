9e63314ef3f4d9a08df53c9eb5bb743729a7e21b
taramunde1974 <120246309+taramunde1974@users.noreply.github.com>
2025-03-27 11:31:43 +0100

    Create header.js

---
+++ js/header.js
@@ -0,0 +1,6 @@
+// script.js
+
+window.addEventListener('resize', function() {
+    // Aquí puedes agregar cualquier lógica que necesites cuando se redimensiona la ventana
+    console.log('Ventana redimensionada');
+});
