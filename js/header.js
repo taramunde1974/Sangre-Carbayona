3f1f9060d915da31ac8d0ad307c765f5ed2224f7
taramunde1974 <120246309+taramunde1974@users.noreply.github.com>
2025-03-28 13:21:09 +0100

    Delete js directory

--- js/header.js
+++
@@ -1,6 +0,0 @@
-// script.js
-
-window.addEventListener('resize', function() {
-    // Aquí puedes agregar cualquier lógica que necesites cuando se redimensiona la ventana
-    console.log('Ventana redimensionada');
-});
